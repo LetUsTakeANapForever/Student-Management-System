e2e40b1723b5f2d551ac0b53302b0f97ac5d688f
LetUsTakeANapForever <kandamkankaw@gmail.com>
2024-09-19 11:43:24 +0700

    Create testDBTable.java

---
+++ testDBTable.java
@@ -0,0 +1,11 @@
+public class testDBTable {
+    public static void main(String[] args) {
+        test t = new test();
+        t.setNum(1);
+        t.setName("Alien1");
+        t.setColor("Blue");
+
+       
+        // save(t);
+    }
+}
