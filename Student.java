5003b9e2b8a0873fa2c72ab3330d681fb6a240d6
Slippery77 <103355666+Slippery77@users.noreply.github.com>
2024-09-19 22:42:37 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,10 +2,12 @@ public class Student  {
     private String Stud_name;
     private String Stud_lastname;
     private String ID;
-    public Student(String id,String n,String ln){
-        this.ID = id;
-        this.Stud_name = n;
-        this.Stud_lastname = ln;
+    private String Password;
+    public Student(String id,String pw,String n,String ln){
+        setID(id);
+        setPassWord(pw);        
+        setStudName(n);
+        setStudLastname(ln);
     }
     public void setID(String id){
         this.ID = id;
@@ -13,22 +15,22 @@ public class Student  {
     public String getID(){
         return this.ID;
     }
-    
+    public void setPassWord(String pw){
+        this.Password = pw;
+    }
+    public String getPassWord(){
+        return this.Password;
+    }
     public void setStudName(String n){
         this.Stud_name = n;
     }
     public String getStudName(){
         return this.Stud_name;
     }
-    
     public void setStudLastname(String ln){
         this.Stud_lastname = ln;
     }
     public String getStudLastname(){
         return this.Stud_lastname;
-    }
-    public static void main(String[] args){
-        Student s1 = new Student("ID" , "Name" , "Lastname");
-        
     }
 }
