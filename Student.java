831375a7ded84e6366a36472c6feb2c26c9b4343
Slippery77 <103355666+Slippery77@users.noreply.github.com>
2024-09-19 22:34:07 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,34 @@
-
+public class Student  {
+    private String Stud_name;
+    private String Stud_lastname;
+    private String ID;
+    public Student(String id,String n,String ln){
+        this.ID = id;
+        this.Stud_name = n;
+        this.Stud_lastname = ln;
+    }
+    public void setID(String id){
+        this.ID = id;
+    }
+    public String getID(){
+        return this.ID;
+    }
+    
+    public void setStudName(String n){
+        this.Stud_name = n;
+    }
+    public String getStudName(){
+        return this.Stud_name;
+    }
+    
+    public void setStudLastname(String ln){
+        this.Stud_lastname = ln;
+    }
+    public String getStudLastname(){
+        return this.Stud_lastname;
+    }
+    public static void main(String[] args){
+        Student s1 = new Student("ID" , "Name" , "Lastname");
+        
+    }
+}
