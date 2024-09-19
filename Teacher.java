4f21f8b4bebd6df445db60d91f58b6ff1c319460
LetUsTakeANapForever <kandamkankaw@gmail.com>
2024-09-19 14:40:27 +0700

    Create Teacher class

--- Teacher.java
+++ Teacher.java
@@ -1,1 +1,44 @@
 
+public class Teacher{
+    private String firstname;
+    private String lastname;
+    private String ID;
+    private String password;
+    // private Subject subject;
+
+    public Teacher(String firstname, String lastname, String ID, String password) { // Subject subject
+        setFirstname(firstname);
+        setLastname(lastname);
+        setID(ID);
+        setPassword(password);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstName) {
+        this.firstname = firstName;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastName) {
+        this.lastname = lastName;
+    }
+    public String getID() {
+        return ID;
+    }
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String grade(){ //Student std, Subject subject
+        // TODO: Create Student class and Subject class
+        return "";
+    }
+}
