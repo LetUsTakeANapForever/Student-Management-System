2f742dc90434534882f64a17df41923520c9bc2c
LetUsTakeANapForever <kandamkankaw@gmail.com>
2024-09-19 11:42:55 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,23 @@
-
+public class test{
+    private int num;
+    private String name;
+    private String color;
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
