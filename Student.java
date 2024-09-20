82101cb65bb92f982aad7b2fa245a7219ec9c4c5
IAMAOD <ougthhh4704@gmail.com>
2024-09-20 13:32:52 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,6 +3,17 @@ public class Student  {
     private String Stud_lastname;
     private String StudId;
     private String Password;
+    private Subject[] SubjectThatRegistered;
+        private int index=0;
+        public Subject[] getAllSubjectThatRegistered() {
+            return SubjectThatRegistered;
+        }
+        public Subject getSubjectThatRegistered(int index) {
+            return SubjectThatRegistered[index];
+        }
+        public void setSubjectThatRegistered(Subject subject) {
+            SubjectThatRegistered[index++] = subject;
+        }
     public Student(String id,String pw,String n,String ln){
         setStudID(id);
         setPassWord(pw);        
