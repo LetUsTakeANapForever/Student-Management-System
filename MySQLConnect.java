912add6e26189e00de534d7f8dbd1dbb8caea3e2
LetUsTakeANapForever <kandamkankaw@gmail.com>
2024-09-20 09:38:14 +0700

    Modify MySQLConnect.java

--- MySQLConnect.java
+++ MySQLConnect.java
@@ -5,23 +5,20 @@ import java.sql.SQLException;
 
 // Test Connection
 public class MySQLConnect {
-    public static void main(String[] args) {
-        // Database credentials
-        String jdbcUrl = "jdbc:mysql://localhost:3306/test"; // form : "jdbc:mysql://localhost:3306/database name"
-        String username = "root"; // MySQL username
-        String password = ""; // MySQL password
+    
+    private static Connection connection;
+    public static Connection getConnection() {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/student_management";
+        String username = "root";
+        String password = "";
 
-        // Establish connection
-        try {
-            @SuppressWarnings("unused") // It's fine, it's intentional.
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password); 
-            // The Connection interface represents a connection to a specific database (in this case, my MySQL database).
-            // DriverManager is a class which responsible for establishing a connection to the database.
-            System.out.println("Connection successful!");
-        } catch (SQLException e) {
-            System.out.println("Connection failed.");
+        try{
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+        }catch (SQLException e){
             e.printStackTrace();
         }
+
+        return connection;
     }
 }
 
