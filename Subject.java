fc62c5ddfd5eba9b5530344be91b2b0001e7c5f4
IAMAOD <ougthhh4704@gmail.com>
2024-09-20 16:28:35 +0700

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,32 +1,31 @@
 public class Subject {
-    private String[] Subject_ID;
-    private String[] Subject_Name;
-    private int index_for_ID=0;
-    private int index_for_Name=0;
+    private String Subject_ID;
+    private String Subject_Name;
+    private String grade;
+    
     
     public Subject(String Subject_ID,String Subject_Name){
          setSubjectID(Subject_ID);
          setSubjectName(Subject_Name);
     }
     public void setSubjectID(String Subject_ID){
-        this.Subject_ID[index_for_ID++] = Subject_ID;   
+        this.Subject_ID = Subject_ID;   
     }
     public void setSubjectName(String Subject_Name){
-        this.Subject_Name[index_for_Name++] = Subject_Name;
-    }
-    public String[] getAllSubjectID(){
-        return Subject_ID;
-    }
-    public String getSubjectID(int index){
-        return Subject_ID[index];
-    }
-
-    public String[] getAllSubjectName(){
-        return Subject_Name;
-    }
-    public String getSubjectName(int index){
-        return Subject_Name[index];
+        this.Subject_Name = Subject_Name;
     }
     
+    public String getSubjectID(){
+        return Subject_ID;
+    }
+    public String getSubjectName(){
+        return Subject_Name;
+    }
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+    public String getGrade() {
+        return grade;
+    }
     
 }
