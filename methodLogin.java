f2e780007d5d96a8144acdd9ba0107c123ac2398
Slippery77 <103355666+Slippery77@users.noreply.github.com>
2024-09-29 00:04:46 +0700

    Update methodLogin.java

--- methodLogin.java
+++ methodLogin.java
@@ -10,15 +10,23 @@
         }
     }
     private boolean checkAccount(String ID,String Password){
-        String connectorURL = "jdbc:mysql://localhost:3306/student-management?user=root&password=sukung34";
+        String connectorURL = "jdbc:mysql://localhost:3306/student-management?user=root&password=sukung34"; // url database ที่อยู่ในเครื่อง 
         try{
-            Connection connection = DriverManager.getConnection(connectorURL);
-            Statement statement = connection.createStatement();
-    
+            // DriverManager เป็นตัวจัดการการเชื่อมต่อกับฐานข้อมูล
+            // Connection เป็นตัวเชื่อมตัวฐานข้อมูล
+            Connection connection = DriverManager.getConnection(connectorURL); //DriverManager สร้างการConnection กับฐานข้อมูล โดยใช้ URL ที่กำหนดไว้ใน connectorURL
+           //Statement ใช้สำหรับส่งคำสั่ง SQL ไปยังฐานข้อมูลและดำเนินการกับข้อมูลที่อยู่ในฐานข้อมูล
+           //createStatement() สร้างวัตถุของ statement ที่เชื่อมกับฐานข้อมูล
+            Statement statement = connection.createStatement(); 
+
+            // ดึงข้อมูลจาก column นักเรียนที่เป็น id/password นักเรียน ว่าตรงกับที่รับค่า ID,Password ไหม
+            // \" ใช้เพื่อหลีกเลี่ยงการตีความเครื่องหมายคำพูดในสตริง ซึ่งทำให้คำสั่ง SQL
             String sql = String.format("SELECT std_id, std_password FROM students WHERE std_id = \"%s\" AND std_password = \"%s\"", ID, Password);
+            
+            //เก็บผลลัพท์ที่ได้จากการดำเนินการของ sql
             ResultSet resultSet = statement.executeQuery(sql);
         
-            return resultSet.next();
+            return resultSet.next(); //return ออกไปเป็น true ถ้ายังสามารถเลื่อนไปตัวต่อไปได้
         }
         catch(Exception e){
             System.out.println(e);
